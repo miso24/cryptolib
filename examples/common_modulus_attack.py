89686004301d7b43129df6f31f5ad0b0bdfb2b45
宮崎想也 <green-24@outlook.jp>
2020-06-07 18:58:55 +0900

    first commit

---
+++ examples/common_modulus_attack.py
@@ -0,0 +1,24 @@
+from cryptolib.number import *
+from cryptolib.encoding import *
+from cryptolib.rsa import *
+
+p = get_prime(512)
+q = get_prime(512)
+
+n = p * q
+e1 = 65537
+e2 = 39317
+#d1 = inverse_mod(e1, lcm(p - 1, q - 1))
+
+m = bytes2long(b'crypto{c0mmon_mo9u1us_a7t5ck}')
+c1 = pow(m, e1, n)
+c2 = pow(m, e2, n)
+
+print(f"c1: {c1}")
+print(f"c2: {c2}")
+
+hack_m = common_modulus_attack(n, e1, e2, c1, c2)
+
+print("-" * 20)
+print(f"Hacked m: {m}")
+print(long2bytes(m).decode())
---
+++ src/number.py
@@ -0,0 +1,147 @@
+import random
+import math
+import secrets
+
+import gmpy2
+
+def lcm(a, b):
+    """LCM
+    
+    calculate lcm
+
+    Args:
+        a: (int)
+        b: (int)
+
+    Returns:
+        int: LCM of a and b
+    """
+    return a * b // math.gcd(a, b)
+
+def exgcd(a, b):
+    """Euclid
+
+    ax + by = gcd(a, b)
+    calculate x and y and gcd(a, b)
+
+    Args:
+        a (int)
+        b (int)
+
+    Returns:
+        int: GCD of a and b
+        int: x
+        int: y
+    """
+    x0, y0, x1, y1 = 0, 1, 1, 0
+    while a != 0:
+        q, a, b = b // a, b % a, a
+        x0, x1 = x1, x0 - x1 * q
+        y0, y1 = y1, y0 - y1 * q
+    return b, x0, y0
+
+def inverse_mod(u, v):
+    """Inverse Mod
+
+    ux ≡ 1 (mod v)
+    calculate x
+
+    Args:
+        u (int) 
+        v (int) 
+
+    Returns:
+        int: x
+    """
+    g, x, _ = exgcd(u, v)
+    if g != 1:
+        raise Exception('mod inverse is not exist!')
+    return x % v
+
+
+def miller_rabin(n, k=20):
+    """miller rabin
+
+    Miller rabin test
+
+    Args:
+        n (long): integer to be tested for primality
+        k (int): the number of rounds of testing
+
+    Returns:
+        bool: is prime
+
+    Examples:
+        >>> miller_rabin(98959625207757469)
+        True
+        >>> miller_rabin(629685412367973552)
+        False
+    """
+    if n == 2: return True
+    if n & 1 == 0 or n == 0: return False
+
+    mpz_n = gmpy2.mpz(n)
+    r, d = 0, mpz_n
+    while d & 1 != 0:
+        d >>= 1
+        r += 1
+
+    for _ in range(k):
+        a = gmpy2.mpz(random.randint(2, mpz_n - 2))
+        x = gmpy2.powmod(a, d, mpz_n)
+        if x == 1 or x == mpz_n - 1:
+            continue
+        for _ in range(r):
+            x = gmpy2.powmod(x, 2, mpz_n)
+            if x != mpz_n - 1:
+                return False
+    return True
+
+def get_randint(n):
+    """
+
+    get random number range 0 ~ n
+
+
+    Args:
+        n (int): max value
+
+    Returns:
+        int: random number (range 0 ~ n)
+    """
+    return random.randint(0, n)
+
+def get_randbits(n):
+    """
+
+    get n bits random number
+
+    Args:
+        n (int): bits size
+
+    Returns:
+        int: n bits random number
+    """
+    return secrets.randbits(n)
+
+def get_prime(n):
+    """
+
+    get n bits prime number
+
+    Args:
+        n (int): bits size
+
+    Returns:
+        int: n bits prime number
+    """
+    p = secrets.randbits(n)
+    while not is_prime(p):
+        p = secrets.randbits(n)
+    return p
+
+def is_coprime(a, b):
+    return math.gcd(a, b) == 1
+
+def is_prime(n):
+    return miller_rabin(n)
