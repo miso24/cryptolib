de2a19cf0987e938bd97250d07997dff8f963a70
宮崎想也 <green-24@outlook.jp>
2020-06-14 17:06:10 +0900

    add setup.cfg
    - rename src to cryptolib

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
 from setuptools import setup
-from setuptools import find_packages
 
-setup(
-    name='cryptolib',
-    version='0.1.0',
-    description='crypto library for CTF',
-    author='miso',
-    packages=['cryptolib'],
-    package_dir={'cryptolib': 'src'},
-    install_requires=['gmpy2']
-)
+setup()
