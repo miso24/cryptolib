d7a22a9f550b941de6f43d6663bec71f697449a1
宮崎想也 <green-24@outlook.jp>
2020-06-20 20:17:31 +0900

    implementation of base58

--- cryptolib/encoding/basex.py
+++ cryptolib/encoding/basex.py
@@ -1,4 +1,10 @@
+from cryptolib.encoding.bytes import long2bytes, bytes2long
+import struct
 import base64
+
+
+_b58table = b'123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+
 
 def b16dec(data):
     """
@@ -51,6 +57,54 @@ def b32enc(data):
         bytes: encoded data
     """
     return base64.b32encode(data)
+
+def b58enc(data):
+    """
+
+    Base58 encode
+
+    Args:
+        data (Union[str, bytes])
+
+    Returns:
+        bytes: encoded data
+    """
+    if isinstance(data, str):
+        data = data.encode()
+
+    l = bytes2long(data)
+    rslt = b''
+
+    while l:
+        rslt = struct.pack('<B', _b58table[l % 58]) + rslt
+        l //= 58
+    return rslt
+
+def b58dec(data):
+    """
+
+    Base58 decode
+
+    Args:
+        data (Union[str, bytes])
+
+    Returns:
+        bytes: decoded data
+    """
+
+    if isinstance(data, str):
+        data = data.encode()
+
+    rslt = b''
+    l = 0
+
+    for byte in data:
+        l = l * 58 + _b58table.index(byte)
+
+    while l:
+        rslt = struct.pack('<B', l % 256) + rslt
+        l >>= 8
+    return rslt
 
 def b64dec(data):
     """
@@ -105,4 +159,3 @@ def b85enc(data):
     """
 
     return base64.b85encode(data)
-
