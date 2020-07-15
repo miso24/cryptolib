4ebe8422d324edb5c5491ec5bea170eca92096d3
宮崎想也 <green-24@outlook.jp>
2020-07-15 11:28:25 +0900

    update pem.py

--- cryptolib/encoding/pem.py
+++ cryptolib/encoding/pem.py
@@ -1,13 +1,15 @@
-from cryptolib.encoding.basex import b64dec, b64enc
 import textwrap
 import re
+
+from cryptolib.encoding.basex import b64dec, b64enc
 
 X509_OLD = "X509 CERTIFICATE"
 X509 = "CERTIFICATE"
 RSA_PRIVATE = "RSA PRIVATE KEY"
 RSA_PUBLIC = "RSA PUBLIC KEY"
 PRIVATE = "PRIVATE KEY"
-PUBLIC = "PUBLIC KEY"
+PUBLIC_KEY = "PUBLIC KEY"
+MARKERS = [X509_OLD, X509, RSA_PRIVATE, RSA_PUBLIC, PUBLIC_KEY]
 
 def encode(data, marker):
     encoded_data = b64enc(data).decode()
@@ -18,13 +20,26 @@ def encode(data, marker):
     pem_data += f"-----END {marker}-----"
     return pem_data
 
-def decode(pem_data, marker):
-    pattern = re.compile(f"-----BEGIN {marker}-----\n(.+?)\n-----END {marker}-----\n?", re.DOTALL)
-    m = pattern.search(pem_data)
+def decode(pem_data):
+    """
 
-    if m is None:
-        raise ValueError("Not a valid")
+    PEM decode
 
-    encoded_data = ''.join(m.group(1).split('\n'))
-    data = b64dec(encoded_data)
-    return data
+    Args:
+        pem_data (str): PEM encoded data
+
+    Returns:
+        Dictionary[bytes]: PEM decoded data
+    """
+    rslt = {marker: [] for marker in MARKERS}
+    for marker in MARKERS:
+        pattern = re.compile(f"-----BEGIN {marker}-----\n(.+?)\n-----END {marker}-----\n?", re.DOTALL)
+        data = pattern.finditer(pem_data)
+
+        if data is None:
+            continue
+
+        for d in data:
+            decoded_data = b64dec(''.join(d.group(1).split('\n')))
+            rslt[marker].append(decoded_data)
+    return rslt
