fbb3d24b58991dc2d77dd0ebde28f09d27cbc784
宮崎想也 <green-24@outlook.jp>
2020-09-20 12:05:20 +0900

    cipher: Add _block_common.py

---
+++ cryptolib/cipher/_block_common.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from typing import List, Callable, Iterator
+from abc import ABCMeta, abstractmethod
+
+EncryptFunc = Callable[[bytes, bytes], bytes]
+DecryptFunc = Callable[[bytes, bytes], bytes]
+
+
+@dataclass
+class BlockCipherAlgo:
+    block_size: int
+    encrypt: EncryptFunc
+    decrypt: DecryptFunc
+
+
+class BlockCipherMode(metaclass=ABCMeta):
+    def __init__(self, key: bytes, cipher_algo: BlockCipherAlgo) -> None:
+        self.key = key
+        self.cipher_algo = cipher_algo
+
+    @abstractmethod
+    def encrypt(self, plain: bytes) -> bytes:
+        pass
+
+    @abstractmethod
+    def decrypt(self, cipher: bytes) -> bytes:
+        pass
+
+
+def split_block(data: bytes, cipher_algo: BlockCipherAlgo) -> Iterator[bytes]:
+    bs = cipher_algo.block_size
+    for i in range(0, len(data) // bs, bs):
+        yield data[i:i+bs]
