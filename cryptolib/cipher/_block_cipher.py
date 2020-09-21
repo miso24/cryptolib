70bd26c2ea421b124799c2d5ba69c41f1dceaca5
宮崎想也 <green-24@outlook.jp>
2020-09-22 01:52:50 +0900

    _block_cipher.py: fix

--- cryptolib/cipher/_block_cipher.py
+++ cryptolib/cipher/_block_cipher.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import overload, TYPE_CHECKING
+from typing import TYPE_CHECKING
 from cryptolib.cipher._ecb import ECBMode
 from cryptolib.cipher._cbc import CBCMode
 from cryptolib.cipher._block_common import (
@@ -12,14 +12,10 @@ if TYPE_CHECKING:
     from cryptolib.cipher._block_common import BlockCipherAlgo, BlockCipherMode
 
 
-@overload
-def create_cipher(key: bytes, algo: BlockCipherAlgo, mode: int, iv: bytes) -> BlockCipherMode:
-    if mode == MODE_CBC:
-        return CBCMode(key, algo, iv)
-    raise ValueError('Invalid mode')
-
-
-def create_cipher(key: bytes, algo: BlockCipherAlgo, mode: int) -> BlockCipherMode:
+def create_cipher(key: bytes, algo: BlockCipherAlgo, mode: int, iv: bytes = None) -> BlockCipherMode:
+    iv = b'\x00' * algo.block_size if iv is None else iv
     if mode == MODE_ECB:
         return ECBMode(key, algo)
+    elif mode == MODE_CBC:
+        return CBCMode(key, algo, iv)
     raise ValueError('Invalid mode')
