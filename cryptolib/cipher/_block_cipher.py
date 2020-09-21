996b4e992bb2f195ebbfa21e0ffb6a0ea82c9f65
宮崎想也 <green-24@outlook.jp>
2020-09-21 10:17:35 +0900

    cipher: Add ECB mode

--- cryptolib/cipher/_block_cipher.py
+++ cryptolib/cipher/_block_cipher.py
@@ -1,15 +1,21 @@
 from __future__ import annotations
 from typing import overload, TYPE_CHECKING
+from cryptolib.cipher._ecb import ECBMode
+from cryptolib.cipher._block_common import (
+    MODE_ECB
+)
+
 
 if TYPE_CHECKING:
     from cryptolib.cipher._block_common import BlockCipherAlgo, BlockCipherMode
 
 
 @overload
-def create_cipher(key: bytes, algo: BlockCipherAlgo, mode: int) -> BlockCipherMode:
-    pass
-
-
-@overload
 def create_cipher(key: bytes, algo: BlockCipherAlgo, mode: int, iv: bytes) -> BlockCipherMode:
     pass
+
+
+def create_cipher(key: bytes, algo: BlockCipherAlgo, mode: int) -> BlockCipherMode:
+    if mode == MODE_ECB:
+        return ECBMode(key, algo)
+    raise ValueError('Invalid mode')
