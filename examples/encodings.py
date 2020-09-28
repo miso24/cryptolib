c570165ba58156128bcb4cdca27940f2fe8954cd
宮崎想也 <green-24@outlook.jp>
2020-09-28 21:14:57 +0900

    examples/encodings.py: Fix

--- examples/encodings.py
+++ examples/encodings.py
@@ -1,12 +1,11 @@
-from cryptolib.encoding.xor import *
-from cryptolib.encoding.bytes import *
-from cryptolib.encoding.basex import *
+from cryptolib.util.binary import *
+from cryptolib.encoding.basex import b16enc, b32enc, b58enc, b64enc, b85enc
 
 m = "Hello,World!"
 
-print(f"xor(key=0x10): {xor(m, 0x10)}")
-print(f"base16: {b16enc(m.encode()).decode()}")
-print(f"base32: {b32enc(m.encode()).decode()}")
-print(f"base64: {b64enc(m.encode()).decode()}")
-print(f"base85: {b85enc(m.encode()).decode()}")
+print(f"Base16: {b16enc(m.encode()).decode()}")
+print(f"Base32: {b32enc(m.encode()).decode()}")
+print(f"Base58: {b58enc(m.encode()).decode()}")
+print(f"Base64: {b64enc(m.encode()).decode()}")
+print(f"Base85: {b85enc(m.encode()).decode()}")
 print(f"long  : {bytes2long(m.encode())}")
