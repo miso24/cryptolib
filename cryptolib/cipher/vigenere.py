3f2c86342fcaeb8dce5b7d89a2aae78bd743f18c
宮崎想也 <green-24@outlook.jp>
2021-02-24 00:55:35 +0900

    vigenere: fix decrypt

--- cryptolib/cipher/vigenere.py
+++ cryptolib/cipher/vigenere.py
@@ -40,14 +40,16 @@ def decrypt(cipher: str, key: str) -> str:
         str
     """
     rslt = ""
-    for idx, c in enumerate(cipher):
-        if not c.isalpha():
+    ctr = 0
+    for c in cipher:
+        if not c.isascii() or not c.isalpha():
             rslt += c
             continue
-        curr_k = key[idx % len(key)]
+        curr_k = key[ctr % len(key)]
         k = upper2idx(curr_k) if curr_k.isupper() else lower2idx(curr_k)
         if c.isupper():
             rslt += idx2upper((upper2idx(c) - k) % 26)
         elif c.islower():
             rslt += idx2lower((lower2idx(c) - k) % 26)
+        ctr += 1
     return rslt
